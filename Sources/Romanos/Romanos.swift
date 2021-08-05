5066a18ba365b25f60cba5d0d7c1a8ddd4790c8a
David Kotlirevsky <david.kotlirevsky@gmail.com>
2021-08-05 15:28:29 -0300

    first commit

--- Sources/Romanos/Romanos.swift
+++ Sources/Romanos/Romanos.swift
@@ -1,3 +1,42 @@
-struct Romanos {
-    var text = "Hello, World!"
+public class Romanos {
+    internal let initialValue: Int
+    public init(initialValue:Int) {
+        self.initialValue = initialValue
+    }
+    
+    public func printRomanos() -> String {
+        var result: String = ""
+        var value: Int = 1
+        
+        while value <= self.initialValue{
+            if value < 4  {
+                result = result +  "\(value): " + String(repeating: "I", count: value)
+            }
+            if value == 4 {
+                result = result + "\(value): IV"
+            }
+            if value >= 5 && value < 9  {
+                let addOneTimes = value - 5
+                result = result +  "\(value): V" + String(repeating: "I", count: addOneTimes)
+            }
+            if value == 9 {
+                result = result + "\(value): IX"
+            }
+            if value >= 10 && value < 14  {
+                let addOneTimes = value - 10
+                result = result +  "\(value): X" + String(repeating: "I", count: addOneTimes)
+            }
+            if value == 14 {
+                result = result + "\(value): XIV"
+            }
+            
+            if value < self.initialValue {
+                let comma: String = ((self.initialValue > 1 && value < self.initialValue ) ? ", " : "")
+                result = result + comma
+            }
+            
+            value+=1
+        }
+        return result;
+    }
 }
--- Tests/RomanosTests/RomanosTests.swift
+++ Tests/RomanosTests/RomanosTests.swift
@@ -1,11 +1,81 @@
     import XCTest
     @testable import Romanos
-
+    
     final class RomanosTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(Romanos().text, "Hello, World!")
+        private var results:Array = ["1: I"]
+        override func setUp() {
+            results.append(results[0] + ", 2: II")
+            results.append(results[1] + ", 3: III")
+            results.append(results[2] + ", 4: IV")
+            results.append(results[3] + ", 5: V")
+            results.append(results[4] + ", 6: VI")
+            results.append(results[5] + ", 7: VII")
+            results.append(results[6] + ", 8: VIII")
+            results.append(results[7] + ", 9: IX")
+            results.append(results[8] + ", 10: X")
+            results.append(results[9] + ", 11: XI")
+            results.append(results[10] + ", 12: XII")
+            results.append(results[11] + ", 13: XIII")
+            results.append(results[12] + ", 14: XIV")
         }
+        
+        fileprivate func evaluatefor(value: Int) {
+            let result: String = Romanos(initialValue: value).printRomanos()
+            print(result)
+            XCTAssertEqual(result, results[value - 1])
+        }
+        
+        func test01() {
+            let value = 1
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test02() {
+            let value = 2
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test03() {
+            let value = 3
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test04() {
+            let value = 4
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test05() {
+            
+            let value = 5
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test06() {
+            let value = 6
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test07() {
+            let value = 7
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test08() {
+            let value = 8
+            XCTAssertEqual(Romanos(initialValue: value).printRomanos(), results[value - 1])
+        }
+        func test09() {
+            let value = 9
+            evaluatefor(value: value)
+        }
+        func test10() {
+            let value = 10; evaluatefor(value: value)
+        }
+        func test11() {
+            let value = 11; evaluatefor(value: value)
+        }
+        func test12() {
+            let value = 12; evaluatefor(value: value)
+        }
+        func test13() {
+            let value = 13; evaluatefor(value: value)
+        }
+        func test14() {
+            let value = 14; evaluatefor(value: value)
+        }
+        
     }
